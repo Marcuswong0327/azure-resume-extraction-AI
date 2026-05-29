8a1e512c7cdbbd4fd4807dd19bccbb5502616ce9
Marcuswong0327 <shiquan0327@gmail.com>
2026-05-29 09:17:39 +0800

    add FullName col when excel export

--- excel_exporter.py
+++ excel_exporter.py
@@ -31,6 +31,7 @@ class ExcelExporter:
             for candidate in candidates_data:
                 row = {
                     'Role Type': candidate.get('role type', ''),
+                    'FullName': candidate.get('full name', ''),
                     'First Name': candidate.get('first name', ''),
                     'Last Name': candidate.get('last name', ''),
                     'Mobile': candidate.get('mobile', ''),
